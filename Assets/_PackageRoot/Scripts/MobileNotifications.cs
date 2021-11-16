9e9d3a69ba97032112cfbf73ba1326fd3d24365e
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2021-11-16 17:34:36 +0200

    Added Schedule method override for TimeSpan offset property

--- Assets/_PackageRoot/Scripts/MobileNotifications.cs
+++ Assets/_PackageRoot/Scripts/MobileNotifications.cs
@@ -10,6 +10,10 @@ namespace Extensions.Notifications
         {
 			Schedule(channel, title, text, DateTime.Now + TimeSpan.FromMilliseconds(250), largeIcon, channelName, channelDescription, smallIcon);
 		}
+		public static void Schedule(string channel, string title, string text, TimeSpan timeOffset, string largeIcon = null, string channelName = "", string channelDescription = "", string smallIcon = null)
+		{
+			Schedule(channel, title, text, DateTime.Now + timeOffset, largeIcon, channelName, channelDescription, smallIcon);
+		}
 		public static void Schedule(string channel, string title, string text, DateTime fireTime, string largeIcon = null, string channelName = "", string channelDescription = "", string smallIcon = null)
 		{
 			if (string.IsNullOrEmpty(smallIcon)) smallIcon = Config.iconSmall;
